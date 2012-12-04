8070bfb9d6c33bfd5c510410929260d282accc51
David Thornton <david@needle.com>
2012-12-04 11:13:26 -0700

    Added new attribute logstash:beaver:user in attributes/beaver.rb

--- attributes/beaver.rb
+++ attributes/beaver.rb
@@ -1,9 +1,11 @@
 
-default['logstash']['beaver']['repo'] = "git+git://github.com/josegonzalez/beaver.git#egg=beaver"
+default['logstash']['beaver']['repo'] = "git+git://github.com/needle/beaver.git#egg=beaver"
 
 default['logstash']['beaver']['server_role'] = "logstash_server"
 
 default['logstash']['beaver']['server_ipaddress'] = nil
+
+default['logstash']['beaver']['user'] = "root"
 
 default['logstash']['beaver']['inputs'] = []
 default['logstash']['beaver']['outputs'] = []
